0c32b056fa27266ac399a16fb53e241c602d8746
Alexandre Oliveira <alexandre.oliv.junior@gmail.com>
2023-05-24 19:54:26 -0300

    primeiro upload

---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,23 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReajusteServiceTest {
+
+    @Test
+    public void reajusteDeveriaSerDe3PorCentroQuandoDesempenhoForADesejar(){
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Alexandre", LocalDate.now(), new BigDecimal("1000"));
+
+        service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+    }
+}
